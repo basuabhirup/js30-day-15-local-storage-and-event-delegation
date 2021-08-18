0a1d930729feb1da9235221fb55ca3c15cc67444
basuabhirup <basu.abhirup@yahoo.com>
2021-08-18 21:29:50 +0530

    Add README + Favicon + 3 extra buttons with modified styles + screenshot

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem("items")) || [];
+const clearAllButton = document.querySelector('[name=clearAll]');
+const checkAllButton = document.querySelector('[name=checkAll]');
+const uncheckAllButton = document.querySelector('[name=uncheckAll]');
 
-const populateList = () => {
+
+const populateList = (itemsList, items) => {
   itemsList.innerHTML = items.map((item, i) => {
     return `<li>
             <input type="checkbox" data-index=${i} id="item${i}" ${item.done ? "checked": ""}>
@@ -19,7 +23,7 @@ const addNewItem = (e) => {
     done: false
   }
   items.push(item);
-  populateList();
+  populateList(itemsList, items);
   localStorage.setItem("items", JSON.stringify(items));
   addItems.reset();
 }
@@ -27,10 +31,31 @@ const addNewItem = (e) => {
 const toggleCheck = (e) => {
   if(!e.target.matches("input")) return;
   items[e.target.dataset.index]["done"] = e.target.checked;
-  populateList();
+  populateList(itemsList, items);
   localStorage.setItem("items", JSON.stringify(items));
 }
 
-populateList();
+const clearAll = () => {
+  items.splice(0, items.length);
+  populateList(itemsList, items);
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+const checkAll = () => {
+  let newItems = items.map(item => ({name: item.name, done: true}));
+  populateList(itemsList, newItems);
+  localStorage.setItem("items", JSON.stringify(newItems));
+}
+
+const uncheckAll = () => {
+  let newItems = items.map(item => ({name: item.name, done: false}));
+  populateList(itemsList, newItems);
+  localStorage.setItem("items", JSON.stringify(newItems));
+}
+
+populateList(itemsList, items);
 addItems.addEventListener("submit", addNewItem);
 itemsList.addEventListener("click", toggleCheck);
+clearAllButton.addEventListener("click", clearAll);
+checkAllButton.addEventListener("click", checkAll);
+uncheckAllButton.addEventListener("click", uncheckAll);
